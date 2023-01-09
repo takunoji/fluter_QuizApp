314badd94768e7c15e468f1298ecf2ffc6ba6867
takunoji <taku.cube@gmail.com>
2023-01-09 20:46:53 +0900

    first commit

---
+++ lib/answer.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart'
+;
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+  final String answerText;
+
+  Answer(this.selectHandler,this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+        child: Text(answerText,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
